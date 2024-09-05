c138538cdd46c49fa2112379cb21c0e07caf904b
KateGomel <ykatuay@gmail.com>
2024-09-05 21:31:31 +0300

    feat: complected pagination

---
+++ scripts/showModalPagination.js
@@ -0,0 +1,77 @@
+import { LINKS_IMG } from "./constants.js";
+import { dataCards } from "./arrCard.js";
+import { showCardPagination } from "./showCardsPagination.js";
+
+const overlay = document.querySelector(".modal"),
+  bodyOverlay = document.querySelector("html");
+
+function closeModalBtn() {
+  overlay.classList.remove("modal-open");
+  bodyOverlay.classList.remove("stop-scroll");
+}
+
+function closeModalOverlay(evt) {
+  if (evt.target === overlay) {
+    overlay.classList.remove("modal-open");
+    bodyOverlay.classList.remove("stop-scroll");
+  }
+}
+
+function showModalPagination(name) {
+  overlay.classList.add("modal-open");
+
+  const currentItem = dataCards.find((item) => item.name === name);
+
+  overlay.innerHTML = `
+  <div class="modal__wrapper">
+    <div class="modal__card">
+      <div class="modal__closeBtn btn-round">
+        <img src="${LINKS_IMG.pets}icons/closed.svg" alt="closedModal">
+      </div>
+      <img src="${LINKS_IMG.pets + currentItem.imgMod}" alt="${
+    currentItem.alt
+  }" class="modal__card-img">
+      <div class="modal__content">
+        <h3 class="modal__title h3">${currentItem.name}</h3>
+        <p class="modal__breed h4">${currentItem.type} - ${
+    currentItem.breed
+  }</p>
+        <p class="modal__description h5">${currentItem.description}</p>
+        <ul class="modal__options-list">
+          <li class="modal__options-item">
+            <span class="modal__options-bolt h5-bolt">Age:</span>
+            <span class="modal__options-text h5">${currentItem.age}</span>
+          </li>
+          <li class="modal__options-item">
+            <span class="modal__options-bolt h5-bolt">Inoculations:</span>
+            <span class="modal__options-text h5">${currentItem.inoculations.join(
+              ", "
+            )}</span>
+          </li>
+          <li class="modal__options-item">
+            <span class="modal__options-bolt h5-bolt">Diseases:</span>
+            <span class="modal__options-text h5">${currentItem.diseases.join(
+              ", "
+            )}</span>
+          </li>
+          <li class="modal__options-item">
+            <span class="modal__options-bolt h5-bolt">Parasites:</span>
+            <span class="modal__options-text h5">${currentItem.parasites.join(
+              ", "
+            )}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+`;
+
+  bodyOverlay.classList.add("stop-scroll");
+
+  const modalCloseBtn = document.querySelector(".modal__closeBtn");
+  modalCloseBtn.addEventListener("click", closeModalBtn);
+
+  overlay.addEventListener("click", closeModalOverlay);
+}
+
+export { showModalPagination };
